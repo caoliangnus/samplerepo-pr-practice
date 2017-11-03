3707f3923c00948155bf7cf655f8f13f9bbe2949
caoliangnus <liangcao1213@yahoo.com>
2017-11-03 09:55:32 +0800

    add HelloWord321 Method

--- TimeTable.java
+++ TimeTable.java
@@ -2,4 +2,9 @@ class TimeTable{
     public static void main(string [] args){
         System.out.println("Test 2");
     }
+
+    public static void main(string [] args){
+        System.out.println("Test 3");
+    }
+
 }
