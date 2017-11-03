3caab74413588ee2c0b1bdb84c906ac9881d3190
caoliangnus <liangcao1213@yahoo.com>
2017-11-03 09:50:34 +0800

    add HelloWord2 Method

--- .idea/HelloWorld.java
+++ .idea/HelloWorld.java
@@ -2,4 +2,9 @@ class HelloWorld{
     public static void main(String args[]){
         System.out.println("HELLO World");
     }
+
+    public static void main(String args[]){
+        System.out.println("HELLO World2");
+    }
 }
+
